bb95f608b9e3eb134b63427ba2b7afde1a5a7a94
Michelle Kyalo <muendikyalo@gmail.com>
2023-07-26 13:22:25 +0300

    Done with arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Write your solution here!
+// Defined cats
+const cats = ["Milo","Otis","Garfield"];
+cats;
+
+// added Ralph to the end of the array
+function destructivelyAppendCat(Ralph){
+    cats.push("Ralph");
+}
+cats;
+
+// added Bob to beginning of the array
+function destructivelyPrependCat(Bob){
+    cats.unshift("Bob");
+}
+cats;
+// remove last cat from the array
+
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop("Garfield");
+}
+cats;
+
+//remove first cat from the array
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift("Milo");
+}
+cats;
+
+// appends cat and returns a new array leaving the array unchanged
+function appendCat(Broom){
+    return [...cats, "Broom"] ;
+}
+
+//prepends a cat and returns new array leaving cats array unchanged
+function prependCat(Arnold){
+    return ["Arnold",...cats];
+}
+cats;
+
+// removes last cat in array & returns a new array leaving the cats array unchanged
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+  }
+cats;
+  
+// removes last cat in array & returns a new array leaving the cats array unchanged
+function removeFirstCat(){
+    return cats.slice(1);
+}
+cats;
+
+
+
+
+
+
+
+
+
